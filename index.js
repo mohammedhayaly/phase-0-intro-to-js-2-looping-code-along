4e72a08bce05b652c02ca18f4540ac618c25cf12
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-07-20 01:27:48 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+
+
+function writeCards(names, eventName) {
+  const thankYouMessages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+
+  return thankYouMessages;
+}
+
+
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+const event = "birthday";
+
+const names = ["Charlie", "Samip", "Ali"];
+const eventName = "birthday";
+
+const result1 = writeCards(gifts, event);
+console.log(result1);
+
+countDown(10);
+
+const result2 = writeCards(names, eventName);
+console.log(result2);
